72d6573afe2ebefdbb3737194c225273c0ac8010
yaredka <nigatyka@yahoo.com>
2017-07-12 16:12:13 +0200

    Yared

---
+++ FlightProject/src/FirstClassFoods.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+
+
+public class FirstClassFoods {
+
+}
--- FlightProject/src/FoodService.java
+++ FlightProject/src/FoodService.java
@@ -1,19 +1,75 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class FoodService {
 
+//	public double totalFoodPrice;
+//	public static double runningTotal;
+//	private static double foodItemPrice;
+//	static boolean foodOrdering = true;
+	
+	
+	int choice;
+	
+
+	
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void foodService() {
 		// TODO Auto-generated method stub
 		
 	}
 
 	public static void FirstClassFoodService() {
-		// TODO Auto-generated method stub
+		System.out.println("You have chosen FIRST Class.\nStart by Choosing Appetizer.");
+		
 		
 	}
 
 	public static void EconomyClassFoodService() {
-		// TODO Auto-generated method stub
+		System.out.println("You have chosen ECONOMY Class.");
 		
 	}
+	
 
+	public void start() {
+		System.out.println("Choose your Reservation class by respective number. \n1. First Class. \n2. Economy Class.");
+		
+		while(true){
+			
+			//Take Input
+			int input = sc.nextInt();
+			
+			//Check Input
+			switch (input) {
+			
+			case 1: {
+				
+				FirstClassFoodService();
+				break;
+			}
+			
+			case 2: {
+				
+				
+				break;
+			}
+			
+			case 3: {
+				
+				
+				break;
+			}
+			
+			default: {
+				
+				break;
+			}
+			}
+		}
+	}
+	
 }
