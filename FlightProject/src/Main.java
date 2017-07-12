355f5718f3244e404005e0cb0ffdeb2313db184e
Administrator <Administrator@DESK-FDCJBB2.lexicon.edu.local>
2017-07-12 14:21:26 +0200

    cleanup in main

--- FlightProject/src/Main.java
+++ FlightProject/src/Main.java
@@ -6,23 +6,16 @@ public class Main {
 		Customer c1=new Customer("Melvin", "Tummy Street 11", "13232", "Nacka", "123123123");
 		Customer c2=new Customer("Saga", "This Street 11", "13232", "Nacka", "123123123");
 		Customer c3=new Customer("Johan", "That Other Street 11", "13232", "Nacka", "123123123");
+
+		
 		Booking b1=new Booking(c1);
-	
-		
-		System.out.println(b1.getTotalPrice());
-		
-		Booking b2=new Booking(c2);
-		
-		
-		
+		Booking b2=new Booking(c2);		
 		Booking b3=new Booking(c3);
 		
 		
 		
-		System.out.println(b3.getTotalPrice());
 		
-//		Booking b4=new Booking("Ylva");
-		
+
 		
 	}
 
