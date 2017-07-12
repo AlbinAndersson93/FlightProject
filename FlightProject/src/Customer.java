4e2df12d86f924f2162c19bd50308ba960f34e33
User <User@DESK-8ZQD132.lexicon.edu.local>
2017-07-12 14:43:42 +0200

    Tre raders test

--- FlightProject/src/Customer.java
+++ FlightProject/src/Customer.java
@@ -22,41 +22,26 @@ public class Customer {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getAddress() {
 		return address;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
 
 	public String getZipcode() {
 		return zipcode;
 	}
 
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
 
 	public String getCity() {
 		return city;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
-	}
 
 	public String getCellphone() {
 		return cellphone;
 	}
 
-	public void setCellphone(String cellphone) {
-		this.cellphone = cellphone;
-	}
 	
 	public void setTicket(String voucher) {
 			this.voucher = voucher;
