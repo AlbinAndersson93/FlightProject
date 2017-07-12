2df2ec24376c503c1cb414948fdd6f77e5f9cd77
User <User@DESK-8ZQD132.lexicon.edu.local>
2017-07-12 15:22:28 +0200

    Removed all setters from Ticket class

--- FlightProject/src/Ticket.java
+++ FlightProject/src/Ticket.java
@@ -27,50 +27,28 @@ public class Ticket {
 		this.seat = seat;
 		this.ticket = "ARN1707170900-LAX1707171800-XREF1";
 
-		
 		tDate = new Date();	
 
-		
 	}
 
 	public String getSource() {
 		return source;
 	}
 
-	public void setSource(String source) {
-		this.source = source;
-	}
-
 	public String getDestination() {
 		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
 	}
 
 	public String getDeparture() {
 		return departure;
 	}
 
-	public void setDeparture(String departure) {
-		this.departure = departure;
-	}
-
 	public String getArrival() {
 		return arrival;
 	}
 
-	public void setArrival(String arrival) {
-		this.arrival = arrival;
-	}
-
 	public String getSeat() {
 		return seat;
-	}
-
-	public void setSeat(String seat) {
-		this.seat = seat;
 	}
 
 	public String getTicket() {
